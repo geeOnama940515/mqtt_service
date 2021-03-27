42c8d834f7a19130b5fd7278ef02934d301ead44
geeOnama940515 <66071367+geeOnama940515@users.noreply.github.com>
2021-03-27 21:09:40 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace mqttservice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstNotifier = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // lstNotifier
+            // 
+            this.lstNotifier.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.lstNotifier.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstNotifier.GridLines = true;
+            this.lstNotifier.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.lstNotifier.HideSelection = false;
+            this.lstNotifier.Location = new System.Drawing.Point(0, 0);
+            this.lstNotifier.MultiSelect = false;
+            this.lstNotifier.Name = "lstNotifier";
+            this.lstNotifier.Size = new System.Drawing.Size(544, 335);
+            this.lstNotifier.TabIndex = 0;
+            this.lstNotifier.UseCompatibleStateImageBehavior = false;
+            this.lstNotifier.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Details";
+            this.columnHeader1.Width = 540;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 335);
+            this.Controls.Add(this.lstNotifier);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lstNotifier;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using MQTTnet;
+using MQTTnet.Client.Options;
+using MQTTnet.Extensions.ManagedClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace mqttservice
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region Variables
+        public static ManagedMqttClient mqttClient;
+        public static MqttFactory MqttFactory;
+        #endregion
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            using (var dbContext = new hydroPhonicsDBEntities())
+            {
+                if (dbContext.Database.Exists())
+                {
+                    lstNotifier.Items.Add("Status : Connected at " + DateTime.Now);
+                    //  await ConnectAsync();
+                    await StartMQTT();
+                }
+            }
+        }
+        public async Task StartMQTT()
+        {
+            await ConnectMQTT();
+            await Publish("Test", "Test123");
+            mqttClient.UseConnectedHandler(async e => {           
+            await SubscribeCommand("cmnd/#");
+            await SubscribeHydrophonics("hydrophonics/#");
+            });
+            mqttClient.UseApplicationMessageReceivedHandler(MessageReceived);
+        }
+        private async Task ConnectMQTT()
+        {
+            string clientId = Guid.NewGuid().ToString();
+            string mqttURI = "124.107.183.2";
+            string mqttUser = "roger";
+            string mqttPassword = "password";
+            int mqttPort = 1883;
+            bool mqttSecure = false;
+
+            var messageBuilder = new MqttClientOptionsBuilder()
+              .WithClientId(clientId)
+              .WithCredentials(mqttUser, mqttPassword)
+              .WithTcpServer(mqttURI, mqttPort)
+              .WithCleanSession();
+
+            var options = mqttSecure
+              ? messageBuilder
+                .WithTls()
+                .Build()
+              : messageBuilder
+                .Build();
+
+            var managedOptions = new ManagedMqttClientOptionsBuilder()
+              .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
+              .WithClientOptions(options)
+              .Build();
+
+            mqttClient = (ManagedMqttClient)new MqttFactory().CreateManagedMqttClient();
+            await mqttClient.StartAsync(managedOptions);
+        }
+        private async Task Publish(String Topic, String Payload)
+        {
+            await mqttClient.PublishAsync(Topic, Payload).ConfigureAwait(false);
+        }
+        private async Task SubscribeHydrophonics(String Topic)
+        {
+            await mqttClient.SubscribeAsync(Topic);
+        }
+        private async Task SubscribeCommand(String Topic)
+        {
+            await mqttClient.SubscribeAsync(Topic);
+        }
+        private void MessageReceived(MqttApplicationMessageReceivedEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(() =>
+            {
+                lstNotifier.Items.Add("Topic : " + e.ApplicationMessage.Topic + " Payload : " + Encoding.UTF8.GetString(e.ApplicationMessage.Payload));
+                int i = e.ApplicationMessage.Topic.IndexOf("/") + 1;
+                string _Topic = e.ApplicationMessage.Topic.Substring(i);
+                string _Payload = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                using (var dbContext = new hydroPhonicsDBEntities())
+                {
+                    dbContext.sensors.Add(new sensor{ SensorType =  _Topic, SensorData  = _Payload, DateCreated = DateTime.Now.ToString()});
+                    dbContext.SaveChanges();
+                }
+            }));
+        }
+    }
+}
